6d55eef0d87518c80ddab17a0b1369f2fc96b83d
Heiko Santosh <heiko2u@gmail.com>
2021-08-27 13:52:25 -0400

    Add files via upload

---
+++ clock.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+export XDG_RUNTIME_DIR="/run/user/1000"
+
+
+NOW=$(date +"%H")
+
+if [ "$NOW" -le 7 ]
+
+then
+    mplayer  /home/pi/Desktop/Alexa\ hourly/7am.mp3
+
+ 
+ 
+ elif [ "$NOW" -le 8 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/8am.mp3
+        
+ 
+ 
+ elif [ "$NOW" -le 9 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/9am.mp3
+        
+ 
+elif [ "$NOW" -le 10 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/10am.mp3
+        
+ elif [ "$NOW" -le 11 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/11am.mp3
+        
+ 
+ elif [ "$NOW" -le 12 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/12pm.mp3
+        
+ 
+ elif [ "$NOW" -le 13 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/1pm.mp3
+        
+ 
+ 
+ elif [ "$NOW" -le 14 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/2pm.mp3
+        
+ 
+ 
+ elif [ "$NOW" -le 15 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/3pm.mp3
+        
+ 
+ 
+ elif [ "$NOW" -le 16 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/4pm.mp3
+        
+ 
+ 
+ 
+ elif [ "$NOW" -le 17 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/5pm.mp3
+        
+ 
+ 
+ 
+elif [ "$NOW" -le 18 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/6pm.mp3
+        
+ 
+elif [ "$NOW" -le 19 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/7pm.mp3
+        
+ 
+elif [ "$NOW" -le 20 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/8pm.mp3
+        
+ 
+elif [ "$NOW" -le 21 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/9pm.mp3
+        
+ 
+
+elif [ "$NOW" -le 22 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/10pm.mp3
+        
+ 
+ 
+ elif [ "$NOW" -le 23 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/11pm.mp3
+
+
+elif [ "$NOW" -le 24 ] 
+
+  then   
+ 
+    mplayer  /home/pi/Desktop/Alexa\ hourly/12am.mp3
+        
+         
+ 
+
+
+
+
+
+
+
+
+
+fi      
+
+exit 0
+
